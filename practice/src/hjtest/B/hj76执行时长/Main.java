79acbddf1009738d105cdcea08f65552730ee375
zwh15220693172 <1772084987@qq.com>
2023-08-06 20:55:22 +0800

    zhangwh

--- practice/src/hjtest/B/hj76执行时长/Main.java
+++ practice/src/hjtest/B/hj76执行时长/Main.java
@@ -1,5 +1,6 @@
 package hjtest.B.hj76执行时长;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
@@ -8,8 +9,28 @@ public class Main {
         while (input.hasNextLine()) {
             int cut = Integer.parseInt(input.nextLine());
             int len = Integer.parseInt(input.nextLine());
-
+            int[] ints = getInts(input.nextLine());
+            int remain = 0;
+            int time = 0;
+            for (int anInt : ints) {
+                if (cut > remain + anInt) {
+                    remain = 0;
+                } else {
+                    remain = remain + anInt - cut;
+                }
+                time++;
+            }
+            while (remain > 0) {
+                remain -= cut;
+                time++;
+            }
+            System.out.println(time);
         }
         input.close();
+    }
+
+    private static int[] getInts(String nextLine) {
+        return Arrays.stream(nextLine.split(" "))
+                .mapToInt(Integer::parseInt).toArray();
     }
 }
