e92e19515536cd597274aa4b34c536e56b1eef73
zwh15220693172 <1772084987@qq.com>
2023-08-10 14:12:54 +0800

    zhangwh

---
+++ practice/src/hjtest/B卷200分/hj11数据最节约的备份方法/Main.java
@@ -0,0 +1,34 @@
+package hjtest.B卷200分.hj11数据最节约的备份方法;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        while (input.hasNextLine()) {
+            int[] ints = getInts(input.nextLine());
+            int left = 0;
+            int right = ints.length - 1;
+            int count = 0;
+            while (left <= right) {
+                int cur = ints[right];
+                while (ints[left] + cur <= 500) {
+                    cur+=ints[left];
+                    left++;
+                }
+                right--;
+                count++;
+            }
+            System.out.println(count);
+        }
+        input.close();
+    }
+
+    private static int[] getInts(String nextLine) {
+        return Arrays.stream(nextLine.split(","))
+                .mapToInt(Integer::parseInt)
+                .sorted()
+                .toArray();
+    }
+}
